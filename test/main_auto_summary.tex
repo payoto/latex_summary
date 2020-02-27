payoto/latex_summary

commit 05b9debd550f36d0b2a3b986f4cd52c60c08eae6

Support \newcommand that use triggers
This is to avoid new commands which use one of the command triggers
causing an error; for command triggers to be detected they need to be
put at the start of a line

latex_summary.py
@@ -121,7 +121,7 @@ capture_specifiers = generate_capture_specifiers(
 
 
 def command_name_to_re_string(command):
-    return r"^[^%]*(\\" + command + ".*)"
+    return r"^[\b]*(\\" + command + ".*)"
 
 
 def pattern_name_to_re_string(pattern,
@@ -156,7 +156,7 @@ def build_regex_list(patterns):
 
 def build_file_parse_re(commands):
 
-    patterns = [r"^[^%]*\\" + c + file_capture for c in commands]
+    patterns = [r"^[\b]*\\" + c + file_capture for c in commands]
     temp_file_parse_re = []
 
     for command, regexp in zip(commands, build_regex_list(patterns)):
@@ -164,8 +164,6 @@ def build_file_parse_re(commands):
             "pattern": command,
             "regexp": regexp,
         })
-
-
 
     return temp_file_parse_re
 
@@ -596,8 +594,8 @@ def write_records(
             f.writelines("%s\n" % l for l in records[rec])
             f.write("\n")
 
+def main():
 
-if __name__ == "__main__":
     file_name = sys.argv[1]
 
     parser_args = dict()
@@ -620,3 +618,12 @@ if __name__ == "__main__":
 
     records, counters = parse_file(file_name, **parser_args)
     write_records(records, file_name, **record_writer_args)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        import pdb
+        import traceback
+        traceback.print_exc()
+        pdb.post_mortem()

test/main_auto_summary.tex
@@ -3,8 +3,8 @@
 
 \section{Parser results}
     \begin{itemize}[noitemsep]
-\item \textbf{sections}: 10 were detected.
-\item \textbf{todos}: 9(1) were detected (completed).
+\item \textbf{sections}: 11 were detected.
+\item \textbf{todos}: 10(1) were detected (completed).
 \item \textbf{questions}: 1 were detected.
 \item \textbf{summarys}: 4 were detected.
 \item \textbf{bad references}: 1 were detected.
@@ -12,7 +12,7 @@
 \item \textbf{optional todos}: 1 were detected.
 \item \textbf{plans}: 2 were detected.
 \item \textbf{muddles}: 2 were detected.
-\item \textbf{supervisor(Tom)s}: 4(4) were detected (completed).
+\item \textbf{supervisor(Tom)s}: 6(6) were detected (completed).
     \end{itemize}
 
 \section{Key of colours and item types}
@@ -62,6 +62,8 @@
         % test\import_test/file_import.tex:11
         \item {\color{red}Import a file.} (section~\ref{autosec:10})
         % test\import_test/file_import.tex:11
+        \item {\color{red}Import a file.} (section~\ref{autosec:11})
+        % test\import_test/file_subfile.tex:18
     \end{enumerate}
 
 % Start file : test\main.tex
@@ -223,6 +225,27 @@ This doesn't work yet.
         \item {\color{Gray}Tom: Comment from supervisor. with many dots. (done then eol) [ (completed) supervisor(Tom)]}
         % test\import_test/file_import.tex:15
 % End file : test\import_test/file_import.tex
+% Start file : test\import_test/file_subfile.tex
+    \end{itemize}
+\chapter{Test subfile}
+        % test\import_test/file_subfile.tex:8
+\label{autosec:10}
+This doesn't work yet.
+        % test\import_test/file_subfile.tex:9
+    \begin{itemize}[noitemsep]
+        \item {\color{red}Import a file.}
+        % test\import_test/file_subfile.tex:18
+        \item {\color{WildStrawberry}Tom: Comment from supervisor.}
+        % test\import_test/file_subfile.tex:19
+        \item {\color{WildStrawberry}Tom: Comment from supervisor. with many dots.}
+        % test\import_test/file_subfile.tex:20
+        \item {\color{Gray}Tom: Comment from supervisor. with many dots. (eol then done) [ (completed) supervisor(Tom)]}
+        % test\import_test/file_subfile.tex:21
+        \item {\color{Gray}Tom: Comment from supervisor. with many dots. (done then eol) [ (completed) supervisor(Tom)]}
+        % test\import_test/file_subfile.tex:22
+% End file : test\import_test/file_subfile.tex
+% Start file : test\custom_input.tex
+% End file : test\custom_input.tex
     \end{itemize}
 % End file : test\main.tex
 

test/custom_input.tex
@@ -1,0 +1,11 @@
+\customseccommand{This section is added via a custom line capture section} 
+
+%!FART: This fart is a custom trigger, it should be brown.
+%!DONE_FART: This fart is a custom trigger, it was brown, now it should be gray.
+%!TODO: this todo in in a custom input file.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.

test/main.tex
@@ -1,21 +1,34 @@
 %!TEX root = main.tex
 %!TODO: Early todo, to test issue \#2.
+%!CUSTOM_TRIGGER_LINE: customseccommand; None
+%!EOL_CUSTOM_TRIGGER_PHRASE: FARTS; {'color':'brown', 'count':'fart', 'description': 'A really shit statement'}
+%!CUSTOM_TRIGGER_FILE: customlatexfileread
 \documentclass[]{memoir}
 \usepackage{enumitem}
 \usepackage{import}
+\usepackage{subfiles}
 
 \title{Test document}
 
+\newcommand\customlatexfileread[1]{\input{#1}}
+\newcommand\customseccommand[1]{\section{#1}}
 
 \begin{document}
 
 \maketitle
 
 \input{test_latex_summary}
-\input{test_latex_summary2 }
+\input{test_latex_summary2}
 
 \import{import_test/}{file_import}
 \import{import_test/}{file_import.tex}
 \subfile{import_test/file_subfile}
+
+\customlatexfileread{custom_input}
+
+\customseccommand{This section is added via a custom line capture section in the main } 
+
+%!FART: This fart is a custom trigger in the main file, it should be brown.
+%!DONE_FART: This fart is a custom trigger in the main file, it was brown, now it should be gray.
 
 \end{document}

commit 79c46f48ecc1611f39812779a977e3d0d06061ee

Fix regexp for leading spaces
Fixes a bug introduced in 05b9debd550f36d0b2a3b986f4cd52c60c08eae6

where sections and directives with leading spaces were ignored.

latex_summary.py
@@ -125,7 +125,7 @@ capture_specifiers = generate_capture_specifiers(
 
 
 def command_name_to_re_string(command):
-    return r"^[\b]*(\\" + command + ".*)"
+    return r"^\s*(\\" + command + ".*)"
 
 
 def pattern_name_to_re_string(pattern,
@@ -160,7 +160,7 @@ def build_regex_list(patterns):
 
 def build_file_parse_re(commands):
 
-    patterns = [r"^[\b]*\\" + c + file_capture for c in commands]
+    patterns = [r"^\s*\\" + c + file_capture for c in commands]
     temp_file_parse_re = []
 
     for command, regexp in zip(commands, build_regex_list(patterns)):

test/import_test/file_import.tex
@@ -13,3 +13,7 @@ proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 %!EOL_SUPERVISOR_TOM: Comment from supervisor. with many dots.
 %!EOL_DONE_SUPERVISOR_TOM: Comment from supervisor. with many dots. (eol then done)
 %!DONE_EOL_SUPERVISOR_TOM: Comment from supervisor. with many dots. (done then eol)
+
+    \chapter{Sections with leading spaces}
+
+  %!TODO: Just a todo in the section with leading spaces.

test/main_auto_summary.tex
@@ -3,8 +3,8 @@
 
 \section{Parser results}
     \begin{itemize}[noitemsep]
-\item \textbf{sections}: 11 were detected.
-\item \textbf{todos}: 11(1) were detected (completed).
+\item \textbf{sections}: 13 were detected.
+\item \textbf{todos}: 13(1) were detected (completed).
 \item \textbf{questions}: 1 were detected.
 \item \textbf{summarys}: 4 were detected.
 \item \textbf{bad references}: 1 were detected.
@@ -61,11 +61,15 @@
         % test\test_latex_summary2.tex:8
         \item {\color{red}Import a file.} (section~\ref{autosec:9})
         % test\import_test/file_import.tex:11
-        \item {\color{red}Import a file.} (section~\ref{autosec:10})
-        % test\import_test/file_import.tex:11
+        \item {\color{red}Just a todo in the section with leading spaces.} (section~\ref{autosec:10})
+        % test\import_test/file_import.tex:19
         \item {\color{red}Import a file.} (section~\ref{autosec:11})
+        % test\import_test/file_import.tex:11
+        \item {\color{red}Just a todo in the section with leading spaces.} (section~\ref{autosec:12})
+        % test\import_test/file_import.tex:19
+        \item {\color{red}Import a file.} (section~\ref{autosec:13})
         % test\import_test/file_subfile.tex:18
-        \item {\color{red}this todo in in a custom input file.} (section~\ref{autosec:11})
+        \item {\color{red}this todo in in a custom input file.} (section~\ref{autosec:13})
         % test\custom_input.tex:5
     \end{enumerate}
 
@@ -208,12 +212,19 @@ This doesn't work yet.
         % test\import_test/file_import.tex:14
         \item {\color{Gray}Tom: Comment from supervisor. with many dots. (done then eol) [ (completed) supervisor(Tom)]}
         % test\import_test/file_import.tex:15
+    \end{itemize}
+\chapter{Sections with leading spaces}
+        % test\import_test/file_import.tex:17
+\label{autosec:9}
+    \begin{itemize}[noitemsep]
+        \item {\color{red}Just a todo in the section with leading spaces.}
+        % test\import_test/file_import.tex:19
 % End file : test\import_test/file_import.tex
 % Start file : test\import_test/file_import.tex
     \end{itemize}
 \chapter{Test import}
         % test\import_test/file_import.tex:1
-\label{autosec:9}
+\label{autosec:10}
 This doesn't work yet.
         % test\import_test/file_import.tex:2
     \begin{itemize}[noitemsep]
@@ -227,12 +238,19 @@ This doesn't work yet.
         % test\import_test/file_import.tex:14
         \item {\color{Gray}Tom: Comment from supervisor. with many dots. (done then eol) [ (completed) supervisor(Tom)]}
         % test\import_test/file_import.tex:15
+    \end{itemize}
+\chapter{Sections with leading spaces}
+        % test\import_test/file_import.tex:17
+\label{autosec:11}
+    \begin{itemize}[noitemsep]
+        \item {\color{red}Just a todo in the section with leading spaces.}
+        % test\import_test/file_import.tex:19
 % End file : test\import_test/file_import.tex
 % Start file : test\import_test/file_subfile.tex
     \end{itemize}
 \chapter{Test subfile}
         % test\import_test/file_subfile.tex:8
-\label{autosec:10}
+\label{autosec:12}
 This doesn't work yet.
         % test\import_test/file_subfile.tex:9
     \begin{itemize}[noitemsep]
@@ -251,7 +269,7 @@ This doesn't work yet.
     \end{itemize}
 \customseccommand{This section is added via a custom line capture section} 
         % test\custom_input.tex:1
-\label{autosec:11}
+\label{autosec:13}
     \begin{itemize}[noitemsep]
         \item {\color{brown}This fart is a custom trigger, it should be brown.}
         % test\custom_input.tex:3
@@ -263,7 +281,7 @@ This doesn't work yet.
     \end{itemize}
 \customseccommand{This section is added via a custom line capture section in the main } 
         % test\main.tex:29
-\label{autosec:11}
+\label{autosec:13}
     \begin{itemize}[noitemsep]
         \item {\color{brown}This fart is a custom trigger in the main file, it should be brown.}
         % test\main.tex:31

test/main_auto_summaryonly.tex
@@ -3,7 +3,7 @@
 
 \section{Parser results}
     \begin{itemize}[noitemsep]
-\item \textbf{sections}: 11 were detected.
+\item \textbf{sections}: 13 were detected.
 \item \textbf{summarys}: 4 were detected.
     \end{itemize}
 
@@ -102,18 +102,26 @@ This should match the previous sentence.
 \label{autosec:8}
 This doesn't work yet.
         % test\import_test/file_import.tex:2
+\chapter{Sections with leading spaces}
+        % test\import_test/file_import.tex:17
+\label{autosec:9}
 % End file : test\import_test/file_import.tex
 % Start file : test\import_test/file_import.tex
+\vspace{-36pt}\hspace{11pt}
 \chapter{Test import}
         % test\import_test/file_import.tex:1
-\label{autosec:9}
+\label{autosec:10}
 This doesn't work yet.
         % test\import_test/file_import.tex:2
+\chapter{Sections with leading spaces}
+        % test\import_test/file_import.tex:17
+\label{autosec:11}
 % End file : test\import_test/file_import.tex
 % Start file : test\import_test/file_subfile.tex
+\vspace{-36pt}\hspace{11pt}
 \chapter{Test subfile}
         % test\import_test/file_subfile.tex:8
-\label{autosec:10}
+\label{autosec:12}
 This doesn't work yet.
         % test\import_test/file_subfile.tex:9
 % End file : test\import_test/file_subfile.tex